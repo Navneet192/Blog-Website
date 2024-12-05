cbcc877d7d28848cef7c886ddfae1e31d00d5fca
navneet yadav <105013664+navnt00@users.noreply.github.com>
2024-12-05 12:28:45 +0530

    vercel for deployment

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -8,7 +8,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r*sn^ud-a&(%uvcafd43y=9#0u#j)p)vkus^$+m^$+2qpqd#nv'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
